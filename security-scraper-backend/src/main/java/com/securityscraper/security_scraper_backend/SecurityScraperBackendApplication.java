1290605e0f299029ba5cff7dfeecf2cf2d0076bb
Ahmadou ABDOU SAIDOU <ahmadousaidou1557@gmail.com>
2026-06-28 18:03:10 +0200

    Update project

---
+++ security-scraper-backend/src/main/java/com/securityscraper/security_scraper_backend/SecurityScraperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securityscraper.security_scraper_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityScraperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityScraperBackendApplication.class, args);
+	}
+
+}
---
+++ security-scraper-backend/src/test/java/com/securityscraper/security_scraper_backend/SecurityScraperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityscraper.security_scraper_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityScraperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
